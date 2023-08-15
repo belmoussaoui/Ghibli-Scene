5fa1730635236519670a30683e0d2ebd42e0a79e
belmoussaoui <emb-12@outlook.com>
2023-08-15 13:47:44 +0200

    :sparkles: Add Cube

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("new journey!");
+console.log("hello world");
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+
+scene.add(mesh);
+
+const camera = new THREE.PerspectiveCamera(75, 800 / 600)
+camera.position.z = 3
+scene.add(camera)
+
+
+
+const canvas = document.querySelector('canvas#play')
+
+
+const renderer = new THREE.WebGLRenderer({ canvas: canvas })
+renderer.render(scene, camera)
