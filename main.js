f01353d762be7e2063f026ffa40def7646c66a24
belmoussaoui <emb-12@outlook.com>
2023-08-15 14:59:03 +0200

    fullsize

--- main.js
+++ main.js
@@ -8,9 +8,18 @@ const mesh = new THREE.Mesh(geometry, material)
 
 scene.add(mesh);
 
-const camera = new THREE.PerspectiveCamera(75, 800 / 600)
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+
+
+console.log(sizes);
 
 
 
@@ -18,16 +27,31 @@ const canvas = document.querySelector('canvas#play')
 
 
 const renderer = new THREE.WebGLRenderer({ canvas: canvas })
-mesh.position.x = 2;
+renderer.setSize(sizes.width, sizes.height)
+
+mesh.position.x = 1;
 mesh.rotation.y = Math.PI / 8;
-mesh.scale.set(2, 2, 1);
+// mesh.scale.set(2, 2, 1);
+const controls = new THREE.OrbitControls(camera, canvas);
+
 
 function animate() {
     requestAnimationFrame(animate); //crée une boucle qui permet de dessiner la scene à chaque fois que la page est chargée
     mesh.rotation.x += 0.01;
     mesh.rotation.y += 0.01;
+	controls.update();
 
     renderer.render(scene, camera);
 }
 
 animate();
+
+function onWindowResize() {
+	sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+	camera.aspect = sizes.width / sizes.height
+	camera.updateProjectionMatrix()
+	renderer.setSize(sizes.width, sizes.height)
+}
+
+window.addEventListener('resize', onWindowResize, false)
