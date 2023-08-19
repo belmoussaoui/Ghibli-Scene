04e5a2c823821cf8245f12f96cde3cbf2ba89b2f
belmoussaoui <emb-12@outlook.com>
2023-08-19 20:28:42 +0200

    :sparkles: Shadow

--- main.js
+++ main.js
@@ -11,11 +11,12 @@ scene.add(camera)
 const canvas = document.querySelector('canvas#play')
 const renderer = new THREE.WebGLRenderer({ canvas: canvas })
 renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.shadowMap.enabled = true;
 
 const controls = new THREE.OrbitControls(camera, canvas);
 
 function createFloor() {
-    const geometry = new THREE.CircleGeometry(2.5, 64); // taille, nb segments
+    const geometry = new THREE.CircleGeometry(4, 64); // taille, nb segments
     const material = new THREE.MeshToonMaterial({
         color: 0x86E426,
         side: THREE.DoubleSide
@@ -24,12 +25,14 @@ function createFloor() {
     circle.rotation.x = Math.PI / 2;
     circle.position.y = -1.515;
     scene.add(circle);
+    circle.receiveShadow = true;
 }
 
 function createCylinder() {
     const geometry = new THREE.CylinderGeometry(0.2, 0.5, 3, 100);
     const material = new THREE.MeshToonMaterial({ color: 0xb76935 });
     const cylinder = new THREE.Mesh(geometry, material);
+    cylinder.castShadow = true;
     scene.add(cylinder);
 }
 
@@ -37,6 +40,7 @@ function createCone() {
     const geometry = new THREE.ConeGeometry(5, 20, 32);
     const material = new THREE.MeshToonMaterial({ color: 0x188E2A });
     const cone = new THREE.Mesh(geometry, material);
+    cone.castShadow = true;
     cone.scale.x = 0.3;
     cone.scale.y = 0.12;
     cone.position.y = 1.5;
@@ -48,7 +52,8 @@ function createLight() {
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
     scene.add(ambientLight);
     const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3);
-    directionalLight.position.set(-1, 0.25, 0);
+    directionalLight.position.set(-2, 1, 0);
+    directionalLight.castShadow = true
     scene.add(directionalLight);
     const pointLight = new THREE.PointLight(0xff9000, 0.5);
     pointLight.position.set(-1, -0.5, 1);
