adff227bb064f219fbf7ee70af51b2457dd2d952
belmoussaoui <emb-12@outlook.com>
2023-08-24 09:47:09 +0200

    :deciduous_tree: Fix tree

--- main.js
+++ main.js
@@ -1,44 +1,52 @@
+THREE.ColorManagement.enabled = false;
+
 const scene = new THREE.Scene();
 
-
 // setup camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
 camera.position.z = 5;
 camera.position.y = 1;
-scene.add(camera)
+scene.add(camera);
 
 // setup render
-const canvas = document.querySelector('canvas#play')
-const renderer = new THREE.WebGLRenderer({ canvas: canvas })
-renderer.setSize(window.innerWidth, window.innerHeight)
+const canvas = document.querySelector("canvas#play");
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
-const gltfLoader = new THREE.GLTFLoader();
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// camera
 const controls = new THREE.OrbitControls(camera, canvas);
 
-gltfLoader.load(
-    '/models/tree.glb',
-    (gltf) => {
-        const model = gltf.scene.children[0];
-        console.log('success')
-        console.log(gltf)
-        scene.add(model)
-        model.position.y = 2;
-        model.position.z = 1;
-        model.position.x = 0.75;
-        model.rotation.y = Math.PI / 2;
-        //model.rotation.z = Math.PI / 2;
 
-        model.traverse((mesh) => {
-            mesh.material = new THREE.MeshStandardMaterial({ color: 0x188E2A });
-        });
-    }
-)
+// texture loader
+const textureLoader = new THREE.TextureLoader();
+const gradientTexture = textureLoader.load("./textures/5.jpg");
+const toonMaterial = new THREE.MeshToonMaterial({ color: 0x273e35 });
+gradientTexture.generateMipmaps = false;
+gradientTexture.minFilter = THREE.NearestFilter;
+gradientTexture.magFilter = THREE.NearestFilter;
+toonMaterial.gradientMap = gradientTexture;
+
+const gltfLoader = new THREE.GLTFLoader();
+gltfLoader.load("/models/tree.glb", (gltf) => {
+    const model = gltf.scene;
+    console.log(gltf);
+    scene.add(model);
+    model.position.y = 1.1;
+    model.scale.set(0.5, 0.5, 0.5);
+    model.traverse((mesh) => {
+        mesh.material = toonMaterial;
+        mesh.castShadow = true;
+    });
+});
 
 function createFloor() {
-    const geometry = new THREE.CircleGeometry(4, 64); // taille, nb segments
+    const geometry = new THREE.CircleGeometry(5, 64); // taille, nb segments
     const material = new THREE.MeshToonMaterial({
-        color: 0x86E426,
-        side: THREE.DoubleSide
+        color: 0x556b2f,
+        side: THREE.DoubleSide,
     });
     const circle = new THREE.Mesh(geometry, material);
     circle.rotation.x = Math.PI / 2;
@@ -49,7 +57,7 @@ function createFloor() {
 
 function createCylinder() {
     const geometry = new THREE.CylinderGeometry(0.2, 0.5, 3, 100);
-    const material = new THREE.MeshToonMaterial({ color: 0xb76935 });
+    const material = new THREE.MeshToonMaterial({ color: 0x432f14 });
     const cylinder = new THREE.Mesh(geometry, material);
     cylinder.castShadow = true;
     scene.add(cylinder);
@@ -57,7 +65,7 @@ function createCylinder() {
 
 function createCone() {
     const geometry = new THREE.ConeGeometry(5, 20, 32);
-    const material = new THREE.MeshToonMaterial({ color: 0x188E2A });
+    const material = new THREE.MeshToonMaterial({ color: 0x188e2a });
     const cone = new THREE.Mesh(geometry, material);
     cone.castShadow = true;
     cone.scale.x = 0.3;
@@ -70,18 +78,18 @@ function createCone() {
 function createLight() {
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
     scene.add(ambientLight);
-    const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3);
-    directionalLight.position.set(-2, 1, 0);
-    directionalLight.castShadow = true
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2);
+    directionalLight.position.set(10, 20, 0);
+    directionalLight.castShadow = true;
     scene.add(directionalLight);
-    const pointLight = new THREE.PointLight(0xff9000, 0.5);
-    pointLight.position.set(-1, -0.5, 1);
+    const pointLight = new THREE.PointLight(0xffffff);
+    pointLight.position.set(-1, 5, 1);
     scene.add(pointLight);
 }
 
 createFloor();
 createCylinder();
-//createCone();
+// createCone();
 createLight();
 
 function animate() {
@@ -93,11 +101,11 @@ function animate() {
 animate();
 
 function onWindowResize() {
-    let width = window.innerWidth
-    let height = window.innerHeight
-    camera.aspect = width / height
-    camera.updateProjectionMatrix()
-    renderer.setSize(width, height)
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(width, height);
 }
 
-window.addEventListener('resize', onWindowResize, false)
+window.addEventListener("resize", onWindowResize, false);
