0908ec03034f4ec4750c44c3002f18e8687a7858
belmoussaoui <emb-12@outlook.com>
2023-08-28 19:51:58 +0200

    :sparkles: Debug UI

--- main.js
+++ main.js
@@ -1,42 +1,64 @@
-THREE.ColorManagement.enabled = false;
+import * as dat from "./node_modules/lil-gui/dist/lil-gui.esm.js";
 
 const scene = new THREE.Scene();
 
+const gui = new dat.GUI();
+const parameters = {
+    color: 0x12401d,
+};
+
+const trunk = {
+    color: 0x44341f,
+};
+
 // setup camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
-camera.position.z = 5;
+camera.position.z = 7;
 camera.position.y = 1;
 scene.add(camera);
 
 // setup render
 const canvas = document.querySelector("canvas#play");
-const renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true, antialias: false });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
-renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 // camera
 const controls = new THREE.OrbitControls(camera, canvas);
 
-
 // texture loader
 const textureLoader = new THREE.TextureLoader();
 const gradientTexture = textureLoader.load("./textures/5.jpg");
-const toonMaterial = new THREE.MeshToonMaterial({ color: 0x273e35 });
+const toonMaterial = new THREE.MeshToonMaterial({ color: parameters.color });
 gradientTexture.generateMipmaps = false;
 gradientTexture.minFilter = THREE.NearestFilter;
 gradientTexture.magFilter = THREE.NearestFilter;
 toonMaterial.gradientMap = gradientTexture;
 
+const trunkMaterial = new THREE.MeshToonMaterial({ color: 0x44341f });
+
+gui.addColor(parameters, "color").onChange(() => {
+    toonMaterial.color.set(parameters.color);
+});
+
+gui.addColor(trunk, "color").onChange(() => {
+    trunkMaterial.color.set(trunk.color);
+});
+
 const gltfLoader = new THREE.GLTFLoader();
 gltfLoader.load("/models/tree.glb", (gltf) => {
     const model = gltf.scene;
-    console.log(gltf);
     scene.add(model);
     model.position.y = 1.1;
     model.scale.set(0.5, 0.5, 0.5);
     model.traverse((mesh) => {
+        if (mesh.name == "Cylinder") {
+            mesh.material = trunkMaterial;
+            mesh.castShadow = true;
+
+            return;
+        }
         mesh.material = toonMaterial;
         mesh.castShadow = true;
     });
@@ -44,13 +66,15 @@ gltfLoader.load("/models/tree.glb", (gltf) => {
 
 function createFloor() {
     const geometry = new THREE.CircleGeometry(5, 64); // taille, nb segments
-    const material = new THREE.MeshToonMaterial({
-        color: 0x556b2f,
+    const material = new THREE.ShadowMaterial({
+        color: 0xd3ba89,
         side: THREE.DoubleSide,
+        transparent: true,
     });
+    material.opacity = 1;
     const circle = new THREE.Mesh(geometry, material);
     circle.rotation.x = Math.PI / 2;
-    circle.position.y = -1.515;
+    circle.position.y = -1.83;
     scene.add(circle);
     circle.receiveShadow = true;
 }
@@ -60,25 +84,25 @@ function createCylinder() {
     const material = new THREE.MeshToonMaterial({ color: 0x432f14 });
     const cylinder = new THREE.Mesh(geometry, material);
     cylinder.castShadow = true;
-    scene.add(cylinder);
+    // scene.add(cylinder);
 }
 
 function createCone() {
     const geometry = new THREE.ConeGeometry(5, 20, 32);
-    const material = new THREE.MeshToonMaterial({ color: 0x188e2a });
+    const material = new THREE.MeshToonMaterial({ color: 0x2c1e0b });
     const cone = new THREE.Mesh(geometry, material);
     cone.castShadow = true;
     cone.scale.x = 0.3;
     cone.scale.y = 0.12;
     cone.position.y = 1.5;
     cone.scale.z = 0.3;
-    scene.add(cone);
+    // scene.add(cone);
 }
 
 function createLight() {
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 1);
     scene.add(ambientLight);
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4);
     directionalLight.position.set(10, 20, 0);
     directionalLight.castShadow = true;
     scene.add(directionalLight);
